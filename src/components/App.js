652f841fca4648e92525f5d3a68bcf2078f31d34
ynitin2906 <ynitin2906@gmail.com>
2023-03-04 20:37:55 +0530

    temp

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 import User from "../models/user";
 
 const App = () => {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [allUsers, setAllUsers] = useState([]);
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    const name = event.target.signupName.value;
+    const email = event.target.signupEmail.value;
+    const password = event.target.signupPassword.value;
+    const confirmPassword = event.target.signupConfirmPassword.value;
+
+    if (password != confirmPassword) {
+      return;
+    }
+    const newUser = new User(email, password, name);
+    setAllUsers([...allUsers, newUser]);
+    event.target.reset();
+  };
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const email = event.target.loginEmail.value;
+    const password = event.target.loginPassword.value;
+    const user = allUsers.find((user) => user.email === email);
+    if (user.password === password) {
+      setLoggedInUser(user);
+      event.target.reset();
+    } else {
+      return;
+    }
+  };
+  const handleLogout = () => {
+    setLoggedInUser(null);
+  };
+
   return (
     <div id="main">
       <table id="all-users">
-      <tbody>
+        <tbody>
           <tr>
             <th>Name</th>
             <th>Email</th>
             <th>Password</th>
           </tr>
-          <tr>
-            <td>{/** user's name */}</td>
-            <td>{/** user's email */}</td>
-            <td>{/** user's password */}</td>
-          </tr>
+          {allUsers.map((user, index) => (
+            <tr key={index}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.password}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
-
-      <div>
-        <form className="signup-form">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="signupName" id="signupName" />
-          <label htmlFor="email">Email</label>
-          <input type="email" name="signupEmail" id="signupEmail" />
-          <label htmlFor="password">Password</label>
-          <input type="password" name="signupPassword" id="signupPassword" />
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <input
-            type="password"
-            name="signupConfirmPassword"
-            id="signupConfirmPassword"
-          />
-        </form>
-        <button id="signup-button">Signup</button>
-        <form className="login-styles">
-          <label htmlFor="loginEmail">Email</label>
-          <input id="loginEmail" name="loginEmail" type="email" />
-          <label htmlFor="loginPassword">Password</label>
-          <input id="loginPassword" name="loginPassword" type="password" />
-        </form>
-        <button id="login-button">Login</button>
-      </div>
-
-      <div>
-        <h3 id="username">{/** Logged in user's name */}</h3>
-        <h3 id="email">{/** Logged in user's email */}</h3>
-        <button id="logout-button">Logout</button>
-      </div>
+      {loggedInUser ? (
+        <div>
+          <h1 id="username">{loggedInUser.name}</h1>
+          <h1 id="email">{loggedInUser.email}</h1>
+          <button id="logout-button" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <form className="signup-form" onSubmit={handleSignUp}>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="signupName" id="signupName" />
+            <label htmlFor="email">Email</label>
+            <input type="email" name="signupEmail" id="signupEmail" />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="signupPassword" id="signupPassword" />
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              name="signupConfirmPassword"
+              id="signupConfirmPassword"
+            />
+            <button id="signup-button">Signup</button>
+          </form>
+          <form className="login-styles" onSubmit={handleLogin}>
+            <label htmlFor="loginEmail">Email</label>
+            <input id="loginEmail" name="loginEmail" type="email" />
+            <label htmlFor="loginPassword">Password</label>
+            <input id="loginPassword" name="loginPassword" type="password" />
+            <button id="login-button">Login</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
